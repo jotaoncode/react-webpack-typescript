fc466ef2576cffac42cd726a5f42cc892d4179b7
jotaoncode <garcia_juan_j@yahoo.com.ar>
2016-07-12 20:02:54 -0300

    added typings and about page

---
+++ day-1/src/components/about.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class About extends React.Component<void, void> {
+    render() {
+        return <p>This application is to learn about React</p>;
+    }
+}
