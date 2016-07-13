4255989e2bb08180a9fae4d9ddabe253e0ee35b5
H3rN4n <desouza030689@gmail.com>
2016-07-13 13:05:54 -0300

    react-router implemented

--- day-2-not-resolved/src/index.tsx
+++ day-2-not-resolved/src/index.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import { Application } from "./components/application";
+import {About} from './components/about';
 
 ReactDOM.render(
-    <Application />,
-    document.getElementById("example")
+  <Router history={hashHistory}>
+  <Route path="/" component={Application}/>
+  <Route path="/about" component={About}/>
+  </Router>,
+  document.getElementById("example")
 );
--- day-2-not-resolved/typings/index.d.ts
+++ day-2-not-resolved/typings/index.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="globals/react/index.d.ts" />
 /// <reference path="modules/flux/index.d.ts" />
 /// <reference path="modules/react-addons-update/index.d.ts" />
+/// <reference path="modules/react-router/index.d.ts" />
