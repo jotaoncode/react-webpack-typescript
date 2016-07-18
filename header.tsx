2755c1823d029eedec3ff322f01b1adad772a294
Guillermo Eyherabide <geyherabide@belatrix.com>
2016-07-18 12:32:19 -0300

    Please enter the commit message for your changes. Lines starting

--- header.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export interface IText { text: string }
-
-export class Header extends React.Component<IText, void> {
-    render() {
-        return <h1>{this.props.text}</h1>
-    }
-}
