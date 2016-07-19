a20bd26bfa425a06387397bfb2c469908e2bd06a
jotaoncode <garcia_juan_j@yahoo.com.ar>
2016-07-19 14:19:19 -0300

    Removed an a reducer not needed inside directors reducers

--- day-4/ecma6/src/reducers/directors.js
+++ day-4/ecma6/src/reducers/directors.js
@@ -1,23 +1,5 @@
 import { browserHistory } from 'react-router'
 
-const director = (action) => {
-  switch (action.type) {
-    case 'REMOVE_DIRECTOR':
-      return {
-        id: action.id,
-        name: action.name,
-        sex: action.sex,
-        nationality: action.nationality,
-        city: action.city,
-        dob: action.dob,
-        age: action.age,
-        blockbuster: action.blockbuster
-      }
-    default:
-      return state
-  }
-}
-
 const directors = (directors = [], action) => {
   switch (action.type) {
     case 'REMOVE_DIRECTOR':
